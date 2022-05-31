ff5bdb22061846a1f9d90115c95139e7c340712b
FractumSeraph <fallenarch@protonmail.ch>
2022-05-30 20:13:24 -0500

    Moved the store code to an environment variable. Removed mulyiple print statements that were used in testing.

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from sqlite3 import Error
 from time import sleep
 from splinter import Browser
-
-import json
+import os
 
 from configparser import ConfigParser
 config = ConfigParser()
-
-import os
-
-import sqlite3
 
 from telegram.ext.updater import Updater
 from telegram.update import Update
@@ -19,15 +14,9 @@ from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
 botfathercode = os.environ['BOTFATHERCODE']
-
-try:
-    conn = sqlite3.connect('database.db')
-except Error as e:
-    print(e)
+storecode = os.environ['STORECODE']
 
 config.read('config.ini')
-config.set('score', '012345', '1')
-print(config.get('score', '012345'))
 with open('config.ini', 'w') as f:
     config.write(f)
 
@@ -36,11 +25,11 @@ def submit_survey(number, comment):
     try:
         browser = Browser('firefox', headless=True)  # defaults to firefox
         browser.visit('http://dunkinrunsonyou.com/')
-        print("Browser is loading Dunkin")
+        # print("Browser is loading Dunkin")
         sleep(1)
         browser.fill('spl_q_inrest_rcpt_code_txt', number)
         browser.find_by_name('forward_main-pager').click()
-        print("Completing survey")
+        # print("Completing survey")
 
         sleep(1)
 
@@ -63,8 +52,8 @@ def submit_survey(number, comment):
 
         browser.find_by_id("onf_q_inrest_rcpt_additional_questions_alt_2").click()
         browser.find_by_id("buttonNext").click()
-        print("Survey " + number + " is complete")
-        sleep(1)
+        # print("Survey " + number + " is complete")
+        sleep(3)
     except:
         pass
 
@@ -94,18 +83,13 @@ def unknown(update: Update, context: CallbackContext):
 
 def add_to_list(update: Update, context: CallbackContext):
     user = update.message.from_user
-    print("Conversation started with " + str(user.id))
-    print('You talk with user {} and his user ID: {} '.format(user['username'], user['id']))
-    print(user)
-    print(len(context.args))
+    print("Conversation started with " + str(user.username) + " " + str(user.id))
     index: int
     for index, line in enumerate(context.args):
-        print("\nIteration number " + str(index + 1))
         line = line + " "
         sanitized_code = sanitize_code(line)
         if sanitized_code != "":
             submit_survey(str(sanitized_code).strip(), "")
-            print("Submitted code number " + str(index + 1) + ".")
             if not config.has_option('score', str(user.id)):
                 config.set('score', str(user.id), str(0))
             userscore = int(config.get('score', str(user.id)))
@@ -116,11 +100,10 @@ def add_to_list(update: Update, context: CallbackContext):
             update.message.reply_text(
                 "Success! Code " + sanitized_code + " was completed! \nYour score is now " + str(userscore) + ".")
         else:
-            print("Skipped number " + str(index + 1) + ". \n")
             update.message.reply_text(
                 "Failed! Survey code may be mistyped.")
             continue
-        print("addcodes command is complete.\n")
+        print("/addcodes command for " + str(user.id) + " is complete.\n")
 
 
 def parse_list(update: Update, context: CallbackContext):
@@ -139,21 +122,24 @@ def parse_list(update: Update, context: CallbackContext):
                 update.message.reply_text(
                     "Failed! Survey code may be mistyped." % update.message.text)
                 continue
-        print("Reached the end of the list!")
+        print("Reached the end of list.txt!")
 
 
 def score(update: Update, context: CallbackContext):
-    print("Scoreboard is under construction.")
-    update.message.reply_text("Scoreboard is under construction.")
+    user = update.message.from_user
+    print(str(user.id) + " " + str(user.username) + " has requested the scoreboard.")
+    for key, value in config.items("score"):
+        # print("ID {} has a score of {}".format(key, value))
+        update.message.reply_text("ID {} has a score of {}".format(key, value))
+
 
 def sanitize_code(code):
     if len(code) != 19:
-        print("Survey code should be 18 numbers long, that one was " + str(
-            len(code)) + ". Ignoring")
+        # print("Survey code should be 18 numbers long, that one was " + str(len(code)) + ". Ignoring")
         code = ""
         return code
-    if code[5:10] != "50180":
-        print("Survey code doesn't contain 50180 in the correct place, instead we found " + code[5:10] + " Ignoring.")
+    if code[5:10] != str(storecode):
+        # print("Survey code doesn't contain " + str(storecode) " in the correct place, instead we found " + code[5:10] + " Ignoring.")
         code = ""
         return code
     return code
@@ -173,4 +159,4 @@ updater.dispatcher.add_handler(MessageHandler(
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
 
 updater.start_polling()
-print("Script has reached the bottom and the updater is polling")
+print("Script has reached the bottom and the updater is polling\n")
