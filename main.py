ae8844d0774897cd7ba57866079033f7640b1ab1
FractumSeraph <fallenarch@protonmail.ch>
2022-05-31 21:21:04 -0500

    Added SQL database storage for codes. Moving from the configparser to sqlite3 next.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from sqlite3 import Error
+import sqlite3
+from datetime import date
+
 from time import sleep
 from splinter import Browser
 import os
 
 from configparser import ConfigParser
+
 config = ConfigParser()
 
 from telegram.ext.updater import Updater
@@ -15,6 +19,25 @@ from telegram.ext.filters import Filters
 
 botfathercode = os.environ['BOTFATHERCODE']
 storecode = os.environ['STORECODE']
+
+conn = sqlite3.connect('dunkin.db', check_same_thread=False)
+cur = conn.cursor()
+# cur.execute("YOUR-SQL-QUERY-HERE;")
+cur.execute("""CREATE TABLE IF NOT EXISTS codes ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+    code INT,
+    monthfromsurvey INT,
+    datefromsurvey INT,
+    timefromsurvey INT,
+    datesubmitted INT,
+    completedby INT)""")
+cur.execute("""CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+    teleid INT,
+    teleusername TEXT,
+    telefirst TEXT,
+    telelast TEXT,
+    score INT,
+    dailyscore INT)""")
+conn.commit()
 
 config.read('config.ini')
 with open('config.ini', 'w') as f:
@@ -68,7 +91,8 @@ def start(update: Update, context: CallbackContext):
 
 
 def help(update: Update, context: CallbackContext):
-    update.message.reply_text("Use the /addcodes command to submit surveys. For example \n /addcodes 012345678901234567 \n Use the /score command to show the scoreboard.")
+    update.message.reply_text(
+        "Use the /addcodes command to submit surveys. For example \n /addcodes 012345678901234567 \n Use the /score command to show the scoreboard.")
 
 
 def unknown_text(update: Update, context: CallbackContext):
@@ -93,12 +117,14 @@ def add_to_list(update: Update, context: CallbackContext):
             if not config.has_option('score', str(user.id)):
                 config.set('score', str(user.id), str(0))
             userscore = int(config.get('score', str(user.id)))
-            userscore +=1
+            userscore += 1
             config.set('score', str(user.id), str(userscore))
             with open('config.ini', 'w') as f:
                 config.write(f)
             update.message.reply_text(
                 "Success! Code " + sanitized_code + " was completed! \nYour score is now " + str(userscore) + ".")
+            print("Calling addtodatabse with " + str(sanitized_code) + " " + str(user.id))
+            add_to_database(sanitized_code, str(user.id))
         else:
             update.message.reply_text(
                 "Failed! Survey code may be mistyped.")
@@ -110,7 +136,7 @@ def parse_list(update: Update, context: CallbackContext):
     with open('list.txt') as f:
         index: int
         for index, line in enumerate(f):
-            print("\nIteration number " + str(index +1))
+            print("\nIteration number " + str(index + 1))
             sanitized_code = sanitize_code(line)
             if sanitized_code != "":
                 submit_survey(str(sanitized_code).strip(), "")
@@ -143,6 +169,22 @@ def sanitize_code(code):
         code = ""
         return code
     return code
+
+
+def add_to_database(code,completedby):
+    # cur.execute("""INSERT INTO codes (code,monthfromsurvey, datefromsurvey,timefromsurvey,datesubmitted,completedby) values (123455018012341234,12,2459730.56208,1234,2459730.56201,12345)""")
+
+    today = date.today()
+
+    monthfromcode = code[12:14]
+    datefromcode = code[14:16]
+    timefromsurvey = code[10:12]
+
+    datesubmitted = today.strftime("%d")
+
+    datafordatabase = (int(code),int(monthfromcode), int(datefromcode), int(timefromsurvey), int(datesubmitted), int(completedby))
+    cur.execute("INSERT INTO codes (code,monthfromsurvey, datefromsurvey,timefromsurvey,datesubmitted,completedby) VALUES(?, ?, ?, ?, ?, ?);", datafordatabase)
+    conn.commit()
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
